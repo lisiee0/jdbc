f909687f7f7a706c16b59319a60d39f3f0625f69
Hojun_Yoo <lisiee0@naver.com>
2021-12-29 15:48:46 +0900

    12-29

--- src/com/javaex/ex03/AuthorApp.java
+++ src/com/javaex/ex03/AuthorApp.java
@@ -1,15 +1,30 @@
 package com.javaex.ex03;
 
+import java.util.List;
+
 public class AuthorApp {
 
 	public static void main(String[] args) {
 		
+		List<AuthorVo> list;
 		AuthorDao authorDao= new AuthorDao();
 		
-		AuthorVo vo01= new AuthorVo("이문열", "경북 영양");
-		
+		AuthorVo vo01= new AuthorVo("이문열", "경북 영양");		
 		authorDao.authorInsert(vo01);
-
+		
+		AuthorVo vo02= new AuthorVo("박경리", "경상남도 통영");
+		authorDao.authorInsert(vo02);
+		
+		AuthorVo vo03= new AuthorVo("유시민", "17대 국회의원");
+		authorDao.authorInsert(vo03);
+		
+		System.out.println("---------------------------------------");
+		list= authorDao.authorSelect();
+		for(AuthorVo av: list) {
+			av.showInfo();
+		}
+		System.out.println("---------------------------------------");
+		
 	}
 
 }
