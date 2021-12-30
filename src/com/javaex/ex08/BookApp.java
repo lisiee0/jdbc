90928ed5307f8eeb154c2b9dbbaf4e1be9d933c0
Hojun_Yoo <lisiee0@naver.com>
2021-12-30 14:49:31 +0900

    ex08 +search

--- src/com/javaex/ex08/BookApp.java
+++ src/com/javaex/ex08/BookApp.java
@@ -7,35 +7,60 @@ public class BookApp {
 
 	public static void main(String[] args) {
 		
-		List<BookVo> list;
+		List<BookVo> bookList;
 		BookDao bookDao= new BookDao();
+		
+		List<AuthorVo> authorList;
+		AuthorDao authorDao= new AuthorDao();
+		
 		Scanner sc= new Scanner(System.in);
 		
+		// 작가등록
+		AuthorVo vo01= new AuthorVo("이문열", "경북 영양");		
+		authorDao.authorInsert(vo01);
+		
+		AuthorVo vo02= new AuthorVo("박경리", "경상남도 통영");
+		authorDao.authorInsert(vo02);
+		
+		AuthorVo vo03= new AuthorVo("유시민", "17대 국회의원");
+		authorDao.authorInsert(vo03);
+		
+		AuthorVo vo04= new AuthorVo("기안84", "기안동에서 산 84년생");		
+		authorDao.authorInsert(vo04);
+		
+		AuthorVo vo05= new AuthorVo("강풀", "온라인 만화가 1세대");
+		authorDao.authorInsert(vo05);
+		
+		AuthorVo vo06= new AuthorVo("김영하", "알쓸신잡");
+		authorDao.authorInsert(vo06);
+		
+		AuthorVo vo07= new AuthorVo("이고잉", "개발자");
+		authorDao.authorInsert(vo07);
 
 		// 책등록
-		BookVo vo01= new BookVo("우리들의 일그러진 영웅", "다림", "1998-02-22", 1);		
-		bookDao.bookInsert(vo01);
+		BookVo bo01= new BookVo("우리들의 일그러진 영웅", "다림", "1998-02-22", 1);		
+		bookDao.bookInsert(bo01);
 		
-		BookVo vo02= new BookVo("삼국지", "민음사", "2002-03-01", 1);		
-		bookDao.bookInsert(vo02);
+		BookVo bo02= new BookVo("삼국지", "민음사", "2002-03-01", 1);		
+		bookDao.bookInsert(bo02);
 		
-		BookVo vo03= new BookVo("토지", "마로니에북스", "2012-08-15", 2);		
-		bookDao.bookInsert(vo03);
+		BookVo bo03= new BookVo("토지", "마로니에북스", "2012-08-15", 2);		
+		bookDao.bookInsert(bo03);
 		
-		BookVo vo04= new BookVo("유시민의 글쓰기 특강", "생각의길", "2015-04-01", 3);		
-		bookDao.bookInsert(vo04);
+		BookVo bo04= new BookVo("자바프로그래밍 입문", "위키북스", "2015-04-01", 7);		
+		bookDao.bookInsert(bo04);
 		
-		BookVo vo05= new BookVo("패션왕", "중앙북스(books)", "2012-02-22", 4);		
-		bookDao.bookInsert(vo05);
+		BookVo bo05= new BookVo("패션왕", "중앙북스(books)", "2012-02-22", 4);		
+		bookDao.bookInsert(bo05);
 		
-		BookVo vo06= new BookVo("순정만화", "재미주의", "2011-08-03", 5);		
-		bookDao.bookInsert(vo06);
+		BookVo bo06= new BookVo("순정만화", "재미주의", "2011-08-03", 5);		
+		bookDao.bookInsert(bo06);
 		
-		BookVo vo07= new BookVo("오직두사람", "문학동네", "2017-05-04", 6);		
-		bookDao.bookInsert(vo07);
+		BookVo bo07= new BookVo("오직두사람", "문학동네", "2017-05-04", 6);		
+		bookDao.bookInsert(bo07);
 		
-		BookVo vo08= new BookVo("26년", "재미주의", "2012-02-04", 4);		
-		bookDao.bookInsert(vo08);
+		BookVo bo08= new BookVo("26년", "재미주의", "2012-02-04", 4);		
+		bookDao.bookInsert(bo08);
 		
 		/*
 		// 책수정
@@ -48,8 +73,8 @@ public class BookApp {
 		
 		// 책리스트 출력
 		System.out.println("-------------------------------------------------");
-		list= bookDao.bookSelect();
-		for(BookVo bv: list) {
+		bookList= bookDao.bookSelect();
+		for(BookVo bv: bookList) {
 			bv.showInfo();
 		}
 		System.out.println("-------------------------------------------------");
@@ -58,7 +83,7 @@ public class BookApp {
 		
 		String search= sc.nextLine();
 
-		for(BookVo bv: list) {
+		for(BookVo bv: bookList) {
 			if(bv.getTitle().contains(search) || bv.getPubs().contains(search) 
 			|| bv.getAuthorName().contains(search)) {
 				bv.showInfo();
