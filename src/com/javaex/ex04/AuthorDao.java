7443b1b89e603bd321d58b62ba7867fcde2b96e4
Hojun_Yoo <lisiee0@naver.com>
2021-12-30 11:10:18 +0900

    12-30

--- src/com/javaex/ex04/AuthorDao.java
+++ src/com/javaex/ex04/AuthorDao.java
@@ -11,6 +11,11 @@ import java.util.List;
 
 public class AuthorDao {
 	
+	// 0. import java.sql.*;
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
 	private String driver= "oracle.jdbc.driver.OracleDriver";
 	private String url= "jdbc:oracle:thin:@localhost:1521:xe";
 	private String id= "webdb";
@@ -19,21 +24,47 @@ public class AuthorDao {
 	public AuthorDao() {
 		
 	}
+
+	private void getConnection() {	
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+			
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+			
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}   
+	}
+	
+	
+	private void close() {
+		// 5. 자원정리
+	    try {
+	        if (rs != null) {
+	            rs.close();
+	        }                
+	        if (pstmt != null) {
+	            pstmt.close();
+	        }
+	        if (conn != null) {
+	            conn.close();
+	        }
+	    } catch (SQLException e) {
+	        System.out.println("error:" + e);
+	    }
+	}
+	
+	
 	
 	public void authorInsert(AuthorVo authorVo) {		
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-
+		this.getConnection();
+		
 		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName(driver);
-
-		    // 2. Connection 얻어오기
-			conn = DriverManager.getConnection(url, id, pw);
-
-		    // 3. SQL문 준비 / 바인딩 / 실행
-		    
+		    // 3. SQL문 준비 / 바인딩 / 실행		    
 			String query= "";
 		    // query= query+"문자열"
 		    query += "insert into author";
@@ -52,39 +83,18 @@ public class AuthorDao {
 		    // 4.결과처리	    
 		    System.out.println(count+ "건이 저장되었습니다. (작가)");
 
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 		    System.out.println("error:" + e);
-		} finally {
-		   
-		    // 5. 자원정리
-		    try {               
-		        if (pstmt != null) {
-		            pstmt.close();
-		        }
-		        if (conn != null) {
-		            conn.close();
-		        }
-		    } catch (SQLException e) {
-		        System.out.println("error:" + e);
-		    }
-		}
+		} 
+		
+		this.close();	
 	}
 	
+	
 	public void authorDelete(int authorId) {
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
+		this.getConnection();
 		
-
 		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName(driver);
-			
-		    // 2. Connection 얻어오기
-			conn = DriverManager.getConnection(url, id, pw);
-			
 		    // 3. SQL문 준비 / 바인딩 / 실행
 		    String query= "";
 		    query += " delete from author ";
@@ -101,39 +111,18 @@ public class AuthorDao {
 		    // 4.결과처리
 		    System.out.println(count+"건이 삭제되었습니다.");
 		     
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 		    System.out.println("error:" + e);
-		} finally {
-		   
-		    // 5. 자원정리
-		    try { 
-		        if (pstmt != null) {
-		            pstmt.close();
-		        }
-		        if (conn != null) {
-		            conn.close();
-		        }
-		    } catch (SQLException e) {
-		        System.out.println("error:" + e);
-		    }
 		}
+		
+		this.close();
 	}
 	
+	
 	public void authorUpdate(AuthorVo authorVo) {
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
+		this.getConnection();
 		
-
 		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName(driver);
-			
-		    // 2. Connection 얻어오기
-			conn = DriverManager.getConnection(url, id, pw);
-			
 		    // 3. SQL문 준비 / 바인딩 / 실행
 			String query= "";
 		    query += " update 	author ";
@@ -154,42 +143,18 @@ public class AuthorDao {
 		    // 4.결과처리
 		    System.out.println(count+"건이 수정되었습니다.");
 		     
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 		    System.out.println("error:" + e);
-		} finally {
-		   
-		    // 5. 자원정리
-		    try { 
-		        if (pstmt != null) {
-		            pstmt.close();
-		        }
-		        if (conn != null) {
-		            conn.close();
-		        }
-		    } catch (SQLException e) {
-		        System.out.println("error:" + e);
-		    }
 		}
+		
+		this.close();
 	}
 	
-	public List<AuthorVo> authorSelect() {
-		
+	public List<AuthorVo> authorSelect() {		
 		List<AuthorVo> authorList= new ArrayList<AuthorVo>();
+		this.getConnection();
 		
-		// 0. import java.sql.*;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
 		try {
-		    // 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName(driver);
-
-		    // 2. Connection 얻어오기
-			conn = DriverManager.getConnection(url, id, pw);
-
 		    // 3. SQL문 준비 / 바인딩 / 실행
 			String query= "";
 			query += " select   author_id, ";
@@ -206,36 +171,21 @@ public class AuthorDao {
 		    // 4.결과처리
 			while(rs.next()) {
 
-	            	int authorId= rs.getInt("author_id"); // 컬럼명이 id로 변경되었기 때문에
-	            	String authorName= rs.getString("author_name");
-	            	String authorDesc= rs.getString("author_desc");
+            	int authorId= rs.getInt("author_id"); // 컬럼명이 id로 변경되었기 때문에
+            	String authorName= rs.getString("author_name");
+            	String authorDesc= rs.getString("author_desc");
 
-	            	
-	            	AuthorVo vo= new AuthorVo(authorId, authorName, authorDesc);
-	            	authorList.add(vo);
-	            }
+            	
+            	AuthorVo vo= new AuthorVo(authorId, authorName, authorDesc);
+            	authorList.add(vo);
+            }
     
-		} catch (ClassNotFoundException e) {
-		    System.out.println("error: 드라이버 로딩 실패 - " + e);
 		} catch (SQLException e) {
 		    System.out.println("error:" + e);
-		} finally {
-		   
-		    // 5. 자원정리
-		    try {
-		        if (rs != null) {
-		            rs.close();
-		        }                
-		        if (pstmt != null) {
-		            pstmt.close();
-		        }
-		        if (conn != null) {
-		            conn.close();
-		        }
-		    } catch (SQLException e) {
-		        System.out.println("error:" + e);
-		    }
 		}
+		
+		this.close();
+		
 		return authorList;
 	}
 }
